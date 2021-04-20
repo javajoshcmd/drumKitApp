e859ed131eedfb8ea883edec970090935ac9712b
Josh Howcroft <Josh@Joshs-iMac.local>
2021-04-20 15:26:15 +0100

    js added for drum kit functionality, minor style changes

---
+++ app.js
@@ -0,0 +1,18 @@
+function playSound(e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    // adds the key pressed into the audio data-key
+    // if matching with html
+    // audio plays
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+  }
+  
+  function removeTransition() {
+    this.classList.remove("playing");
+  }
+  
+  window.addEventListener("keydown", playSound);
+  const keys = Array.from(document.querySelectorAll(".key"));
+  keys.forEach(key => key.addEventListener("transitionend", removeTransition));
