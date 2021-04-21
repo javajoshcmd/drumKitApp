0a9e697b42e70880d7c8ddb205d2bab29709dfbf
Josh Howcroft <Josh@Joshs-iMac.local>
2021-04-21 09:50:25 +0100

    final tweaks to drum audio

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const playSound=(e) => {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    // adds the key pressed into the audio data-key
-    // if matching with html
-    // audio plays
     audio.currentTime = 0;
     audio.play();
     key.classList.add("playing");
